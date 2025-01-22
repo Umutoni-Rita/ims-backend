26f28ec9d4d1b9f6566b2b27a2a4904d6da8c3b4
Umutoni-Rita <umurita37@gmail.com>
2025-01-22 21:36:13 +0200

    Minor changes

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# class CustomUser(AbstractUser):
-#     ROLE_CHOICES = (
-#         ('admin', 'Admin'),
-#         ('employee', 'Employee'),
-#     )
-#     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('employee', 'Employee'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+
+    # Fields for employees
+    department = models.CharField(max_length=100, blank=True, null=True)
+
+    def is_employee(self):
+        return self.role == 'employee'
+
+    def is_admin(self):
+        return self.role == 'admin'
